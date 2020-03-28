1b7e37f6a83dc6c801d608cf9a8d1a8657886b24
Agusti Bau <agustibau@gmail.com>
2020-03-29 00:59:15 +0100

    initial animation approach

--- src/components/background.js
+++ src/components/background.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {TechIcons} from './svg';
+import React, {useState, useEffect} from 'react';
+import { TechIcons } from './svg';
+import posed, { PoseGroup } from 'react-pose';
 
 import '../css/base.scss';
 
@@ -8,30 +9,37 @@ const getRandomValue = (max) => Math.floor(Math.random() * max)
 
 const getRandomIcon = () => TechIcons[getRandomValue(TechIcons.length)];
 
-export default ({color, n_elements, size, opacity}) =>{
-
-
-
+export default ({ color, n_elements, size, opacity }) => {
+    
 
 
     const svgs = [];
     for (let index = 0; index < n_elements; index++) {
         const y = getRandomValue(100);
         const x = getRandomValue(100);
-        svgs.push(<div
+
+        const PosedEl = posed.div({
+            to: { 
+                top: `calc(${y}% - ${size}px)`, 
+                left: `calc(${x}% - ${size}px)`,
+                transition: { duration: 20000 }
+            },
+            from: { top: `50%`, left: '50%', transition: { duration: 20000 } }
+        });
+        svgs.push(<PosedEl
+            initialPose="from" pose="to"
+            key={`svg-${index}`}
             style={{
-            position:"absolute",
-            top: `calc(${y}% - ${size}px)`,
-            left: `calc(${x}% - ${size}px)`,
-            width: size,
-            height:size,
-            opacity: opacity,
+                position: "absolute",
+                width: size,
+                height: size,
+                opacity: opacity,
             }}>
             {getRandomIcon()(color)}
-        </div>)
+        </PosedEl>)
     }
 
-  
+
 
     return <div className="background">{svgs}</div>
 }
